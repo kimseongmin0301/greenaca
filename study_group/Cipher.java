89b42716fea244f2565d4ed7427b1ae9dc0467d9
kimseongmin0301 <hippo159753@gmail.com>
2022-07-04 23:31:09 +0900

    시저암호

--- study_group/Cipher.java
+++ study_group/Cipher.java
@@ -9,11 +9,11 @@ public class Cipher {
         String inPut = sc.next();
         String outPut = "";
 
-        
+        //문자 길이만큼 반복
         for(int i=0; i<inPut.length(); i++){
-            int cipher = (int)inPut.charAt(i);
+            int cipher = (int)inPut.charAt(i);      //첫 문자부터 아스키코드값으로 형변환
             
-            if((cipher > 64 && cipher < 90) || (cipher > 96 && cipher < 123)){
+            if((cipher > 64 && cipher < 90) || (cipher > 96 && cipher < 123)){      //영어만 바뀌게 조건적용
                 cipher = perform.encoding(cipher);
                 // cipher = perform.decoding(cipher);
             }
@@ -24,7 +24,7 @@ public class Cipher {
         System.out.println(outPut);
     }
 
-    private int encoding(int cipher){
+    private int encoding(int cipher){       //암호화
         cipher += 3;
         if(((cipher > 90) && (cipher < 94)) || ((cipher > 122) && (cipher < 126))){
             cipher -= 26;
@@ -33,7 +33,7 @@ public class Cipher {
         return cipher;
     }
 
-    private int decoding(int cipher){
+    private int decoding(int cipher){       //복호화
         cipher -= 3;
         if((cipher < 65 && cipher > 61) || ((cipher < 97) && (cipher > 93))){
             cipher += 26;
