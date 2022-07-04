3319ed84da5f925fb9cde76279eaafe38d19e379
kimseongmin0301 <hippo159753@gmail.com>
2022-07-04 12:45:22 +0900

    객체 Ex

--- java/variable/TAY.java
+++ java/variable/TAY.java
@@ -1,11 +1,32 @@
 package variable;
 
+import Operator.Operator;
+
 public class TAY {
+    public static class Car {
+        String model;
+        int speed;
+        Car(String model) {
+            this.model = model;
+        }
+        void setModel(String model) {
+            this.model = model;
+        }
+        void setSpeed(int speed) {
+            this.speed = speed;
+        }
+        void run() {
+            for(int i = 10; i <= 50; i += 10) {
+                this.setSpeed(i);
+                System.out.println(this.model + "달립니다. (시속 :" + this.speed + "km/h)");
+            }
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("123456");
-        System.out.println("123456");
-        System.out.println("123456");
-        System.out.println("123456");
-        System.out.println("123456");
+        Car myCar = new Car("포르쉐");
+        Car yourCar = new Car("경운기");
+
+        myCar.run();
+        yourCar.run();
     }
 }
