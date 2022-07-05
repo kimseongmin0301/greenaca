5a4c6f435ce3f0bb6bd7ebd53f3b51043e97e974
kimseongmin0301 <hippo159753@gmail.com>
2022-07-05 15:08:53 +0900

    예제 while,switch

--- java/loop/Bank.java
+++ java/loop/Bank.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        boolean banking = true;
 
+        boolean banking = true;
+        int money =0;
+        int withdraw = 0;
+        int total = 0;
         while(banking){
             System.out.println("----------------------------------");
             System.out.println("1. 예금 | 2. 출금 | 3. 조회 | 4. 종료");
@@ -15,16 +18,20 @@ public class Bank {
 
             switch(answer) {
                 case 1:
-                    System.out.println("예금액 : 10000");
+                    System.out.print("예금 금액 : ");
+                    money = sc.nextInt();
+                    total += money;
                     break;
                 case 2:
-                    System.out.println("출금액 : 2000");
+                    System.out.print("출금 금액 : ");
+                    withdraw = sc.nextInt();
+                    total -= withdraw;
                     break;
                 case 3 :
-                    System.out.println("잔고 : 8000");
+                    System.out.print("잔고 : " + total);
                     break;
                 case 4 :
-                    System.out.println("종료하겠습니다.");
+                    System.out.print("종료하겠습니다.");
                     banking = false;
                     break;
             }
