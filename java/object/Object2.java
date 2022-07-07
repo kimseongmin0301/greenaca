d08bfd70086b0a93e90f68d80008d331c0f243ed
kimseongmin0301 <hippo159753@gmail.com>
2022-07-07 16:07:07 +0900

    method 연습

---
+++ java/object/Object2.java
@@ -0,0 +1,9 @@
+package object;
+
+public class Object2 {
+    public static void main(String[] args) {
+        ObjectEx oj = new ObjectEx();
+
+        oj.ease();
+    }
+}
---
+++ java/object/ObjectEx.java
@@ -0,0 +1,18 @@
+package object;
+
+public class ObjectEx {
+    int width = 205;
+    int kyung = 15;
+    int ratio = 75;
+
+    void weight(){
+
+    }
+    void transform(){
+
+    }
+    static void ease(){
+        System.out.print("a");
+    }
+
+}
