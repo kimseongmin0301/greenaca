6e520c27a13dbbbb320da98896202e809ef74a7c
kimseongmin0301 <hippo159753@gmail.com>
2022-07-11 11:20:00 +0900

    HashMap getOrDefault

--- study_group/MapEx.java
+++ study_group/MapEx.java
@@ -8,8 +8,10 @@ public class MapEx {
 
         for(String i : a){
             map.put(i, map.getOrDefault(i , 0)+1);
+            //map에 값이 없었다면 value = 1, 있었다면 value += 1
         }
 
-        System.out.print(map);
+        System.out.println(map);
+        //{A=5, B=4, C=2}
     }
 }
