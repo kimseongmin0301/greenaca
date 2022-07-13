614877a6ad5078a44346131491f44a26c8547006
kimseongmin0301 <hippo159753@gmail.com>
2022-07-13 12:34:57 +0900

    클래스상속 Super()

--- java/inheritance/SuperTest2.java
+++ java/inheritance/SuperTest2.java
@@ -19,8 +19,9 @@ class Point2D{
 class Point3D extends Point2D{
     int z;
 
-    Point3D(int x, int y) {
+    Point3D(int x, int y, int z) {
         super(x, y);
+        this.z = z;
     }
 
     @Override
