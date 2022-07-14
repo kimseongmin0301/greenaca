5a3662fe28f9effc425bb1f33dcd7032d942a8d1
kimseongmin0301 <hippo159753@gmail.com>
2022-07-14 16:43:20 +0900

    0714 캡슐화 문제

---
+++ java/Exam/Exam0714.java
@@ -0,0 +1,26 @@
+package Exam;
+
+public class Exam0714 {
+    public static void main(String[] args) {
+        Account ac = new Account(1000001);
+        System.out.println(ac.getBalance());
+    }
+}
+class Account{
+    private int balance = 10000;
+    final private int MIN_BALANCE = 0;
+    final private int MAX_BALANCE = 1000000;
+
+    Account(int balance){
+       setBalance(balance);
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        if(balance >= MIN_BALANCE && balance <= MAX_BALANCE)
+            this.balance = balance;
+    }
+}
