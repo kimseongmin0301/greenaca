7a457db2a0e3572c7b663408a383b4d244cccc74
kimseongmin0301 <hippo159753@gmail.com>
2022-07-18 15:11:43 +0900

    try catch 예제

--- java/Exam/Exam0718.java
+++ java/Exam/Exam0718.java
@@ -4,29 +4,33 @@ import java.util.Scanner;
 
 public class Exam0718 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         int cnt=0;
         boolean game = true;
         int com = (int) ((Math.random()*100)+1);
+        int user = 0;
 
         while(game){
             System.out.println("1과 100사이의 값을 입력하세요 : ");
 
             try{
-                int user = Integer.parseInt(sc.nextLine());
-                cnt++;
-
-                if(com > user){
-                    System.out.println("더 큰수를 입력하세요");
-                }else if(com < user) {
-                    System.out.println("더 작은수를 입력하세요");
-                }else{
-                    System.out.println("정답");
-                    System.out.println(cnt + "번만에 맞추셨습니다.");
-                    game = false;
-                }
+                Scanner sc = new Scanner(System.in);
+                user = Integer.parseInt(sc.nextLine());
             }catch(NumberFormatException e){
                 System.out.println("숫자만 입력할 수 있습니다. 다시 입력해주세요~");
+                continue;
+            }
+
+            cnt++;
+
+            if(com == user){
+                System.out.println("정답");
+                System.out.println(cnt + "번만에 맞추셨습니다.");
+                game = false;
+            }
+            if(com < user) {
+                System.out.println("더 작은수를 입력하세요");
+            }else{
+                System.out.println("더 큰수를 입력하세요");
             }
         }
     }
