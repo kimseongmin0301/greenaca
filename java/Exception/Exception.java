c6de21c734cb7dccaea35e2b0a8e5f2b0fd3ad61
kimseongmin0301 <hippo159753@gmail.com>
2022-07-18 11:39:54 +0900

    예외처리 try catch

--- java/Exception/Exception.java
+++ java/Exception/Exception.java
@@ -1,5 +1,28 @@
 package Exception;
 
 public class Exception {
+    public static void main(String[] args) {
+        int a = 0,b = 0;
+        try {
+            System.out.println(1);
+            System.out.println(2);
+            System.out.println(3);
+            System.out.println(a/b);
+            System.out.println(4);
+        }
+        catch(ArithmeticException e){
+            System.out.println(5);
+            if(a==0) {
+                a = 1;
+            }
+            if(b==0){
+                b = 1;
+            }
+            System.out.println(a/b);
+        }
+        finally {
+            System.out.println(6);
+        }
 
+    }
 }
