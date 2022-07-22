10f683d62796163b9edef73071f08769648b70d0
kimseongmin0301 <hippo159753@gmail.com>
2022-07-22 15:45:15 +0900

    TCL 트랜젝션 제어어

--- SQL/Transaction.sql
+++ SQL/Transaction.sql
@@ -7,3 +7,14 @@ delete
 
 commit
 rollback
+*/
+
+select * from board;
+
+start transaction;
+	select title, content,cnt from board WHERE id = 'c';
+	update board
+    set cnt = cnt + 1
+    where id = 'c';
+	select title, content,cnt from board WHERE id = 'c';
+rollback;
