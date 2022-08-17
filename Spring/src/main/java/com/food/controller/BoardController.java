f2527890b59ddf20c98164510da384540829c227
kimseongmin0301 <pose1358@naver.com>
2022-08-17 15:32:45 +0900

    오타수정

--- Spring/src/main/java/com/food/controller/BoardController.java
+++ Spring/src/main/java/com/food/controller/BoardController.java
@@ -7,12 +7,9 @@ import com.food.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.ArrayList;
 
 @Controller
 public class BoardController {
